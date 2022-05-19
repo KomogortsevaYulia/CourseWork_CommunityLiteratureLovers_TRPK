89f8f0dddff930333c58629868c2ba4380f7af80
KomogortsevaYulia <komog-y-v@yandex.ru>
2022-05-20 03:18:13 +0800

    +

--- Project/server/routes/users.js
+++ Project/server/routes/users.js
@@ -6,7 +6,6 @@ const {authByToken} = require('../middleware/auth')
 router.post('/users',UserController.createUser)                     //Register a new user
 router.post('/users/login',UserController.loginUser)                //Login for existing user
 router.get('/user/:email',authByToken,UserController.getUserByEmail)       //Gets the currently logged-in user
-router.patch('/user',authByToken,UserController.updateUserDetails)  //Updated user information for current user
 router.get('/user',authByToken,UserController.getUserByEmail)
 
 module.exports = router
