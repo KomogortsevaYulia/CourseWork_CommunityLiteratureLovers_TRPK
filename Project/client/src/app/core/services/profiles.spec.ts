b330f1d5c985d13670b4c0588acaca7cb416eb31
KomogortsevaYulia <komog-y-v@yandex.ru>
2022-06-04 01:13:17 +0800

    осталось доделать функциональный тест

--- Project/client/src/app/core/services/profiles.spec.ts
+++ Project/client/src/app/core/services/profiles.spec.ts
@@ -1,32 +1,32 @@
 import { TestBed } from "@angular/core/testing";
 import { ProfilesService } from "./profiles.service";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
-import { HttpTestingController } from "@angular/common/http/testing";
 import { Profile } from "../models";
 
 describe("ProfilesService", () => {
+  
   let service: ProfilesService;
-  let controller: HttpTestingController;
+  //переменная с которой будем сравнивать
   const profiles: Profile = {
           username: "komogortseva",
           fio: "Комогорцева Юлия Владимировна ",
           email: "komog-y-v@yandex.ru",
           nameRole: "Пользователь",
   };
-
+  //код который надо выполнить перед выполнением теста
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [ProfilesService],
     });
+    //создание обьекта ProfilesService
     service = TestBed.inject(ProfilesService);
-    controller = TestBed.inject(HttpTestingController);
   });
-
+  //тест для проверки создался ли обьект 
   it('Создание обьекта класса ProfilesService', () => {
     expect(service).toBeTruthy();
   });
-
+  //тест для проверки правильно ли работает метод обьекта,возвращает ли данные и правильные ли они
   it("Получение информации для личного кабинета методом get класса ProfilesService", () => {
       service.get("komogortseva").subscribe(data => {
         expect(data).toEqual(profiles);
--- Project/client/src/app/help/help.component.spec.ts
+++ Project/client/src/app/help/help.component.spec.ts
@@ -1,13 +1,11 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing"
 import { RouterTestingModule } from "@angular/router/testing";
 import { SharedModule } from '../shared';
-import { AuthRoutingModule } from "../auth/auth-routing.module";
-import { UserService } from "../core";
 import { HelpComponent } from "./help.component";
 
 describe('help component', () => {
     let fixture: ComponentFixture<HelpComponent>
-    let component:HelpComponent
+    
     beforeEach(async() => {
       await TestBed.configureTestingModule({
         declarations: [HelpComponent],
